cc56fb8a50fb7ef17853d170dbda1ba1cc39a005
dennuguyen <dennuguyen@gmail.com>
2022-03-29 11:11:26 +1100

    Update triangulation and trilateration.

--- triangulation.m
+++ triangulation.m
@@ -1,5 +1,5 @@
-function [x, y] = triangulation(r1, r2, x1, x2, y1, y2)
-    fn = @(u) [sqrt((u(1) - x1)^2 + (u(2) - y1)^2) - r1;
-               sqrt((u(1) - x2)^2 + (u(2) - y2)^2) - r2];
-    [x, y] = fsolve(fn, 
+function [heading] = triangulation(a1, a2, x1, x2, y1, y2, x, y)
+    fn = @(u) [atan2(y1 - y, x1 - x) - a1;
+               atan2(y2 - y, x2 - x) - a2];
+    heading = fsolve(fn, [0 0]);
 end
--- trilateration.m
+++ trilateration.m
@@ -1,3 +1,5 @@
-function [heading] = trilateration(angle1, angle2, x1, x2, y1, y2, x, y)
-    fn = @(u) []
+function [pose] = trilateration(r1, r2, x1, x2, y1, y2)
+    fn = @(u) [sqrt((u(1) - x1)^2 + (u(2) - y1)^2) - r1;
+               sqrt((u(1) - x2)^2 + (u(2) - y2)^2) - r2];
+    pose = fsolve(fn, [0 0]);
 end
